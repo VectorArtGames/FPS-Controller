1730330a5f9ebf7c2df85fc29113df80613fa998
ADEL\spp109 <martin.magnusson@adelfors.nu>
2020-10-13 11:25:14 +0200

    updated

---
+++ src/3D Controller/Assets/Game/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera fov;
+	public Rigidbody rb;
+
+	[Header("Ground Check"), Space(5f)]
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask GroundLayer;
+
+	public float Speed;
+
+	public float RotationDamping;
+
+	private bool CanJump { get; set; }
+
+	public float Sensitivity;
+
+	public float maxYAngle = 80f;
+
+	private Vector2 currentRotation;
+
+	private void Awake()
+	{
+		TryGetComponent(out rb);
+		fov = GetComponentInChildren<Camera>();
+	}
+
+	private void OnEnable()
+	{
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void OnDisable()
+	{
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+	}
+
+	private void Update()
+	{		
+		currentRotation.x -= Input.GetAxisRaw("Mouse Y") * Sensitivity;
+		currentRotation.y += Input.GetAxisRaw("Mouse X") * Sensitivity;
+
+		currentRotation.x = Mathf.Clamp(currentRotation.x, -maxYAngle, maxYAngle);
+		
+
+		if (CanJump && Input.GetButtonUp("Jump"))
+			rb.velocity = new Vector3(0, 20, 0);
+	}
+
+	private void FixedUpdate()
+	{
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, currentRotation.y, 0), RotationDamping * Time.deltaTime);
+		fov.transform.rotation = Quaternion.Lerp(fov.transform.rotation, Quaternion.Euler(currentRotation.x, transform.rotation.eulerAngles.y, 0), RotationDamping * Time.deltaTime);
+
+
+		CanJump = Physics.CheckSphere(GroundCheck.position, GroundCheckRadius, GroundLayer);
+
+		var x = Input.GetAxis("Horizontal");
+		var z = Input.GetAxis("Vertical");
+		var n = fov.transform.rotation.normalized;
+		rb.MovePosition(rb.position + (n * new Vector3(x, 0, z) * Speed * Time.deltaTime));
+	}
+
+	#if UNITY_EDITOR
+
+	private void OnDrawGizmos()
+	{
+		if (GroundCheck == null) return;
+
+		Gizmos.color = CanJump ? Color.white : Color.red;
+
+		Gizmos.DrawWireSphere(GroundCheck.position, GroundCheckRadius);
+	}
+
+	#endif
+}
---
+++ src/3D Controller/Assets/Game/Shader/BW.shader
@@ -0,0 +1,49 @@
+﻿Shader "Hidden/BW" {
+	Properties {
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_bwBlend("Black & White blend", Range(0, 1)) = 0
+	}
+	SubShader {
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			uniform float _bwBlend;
+
+			struct appdata {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f {
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v) {
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			fixed4 frag(v2f_img i) : COLOR {
+				float4 c = tex2D(_MainTex, i.uv);
+
+				float lum = c.r * .3 + c.g * .59 + c.b * .11;
+				float gamma = 1;
+				float v = c.r + c.g + c.b % 255;
+				float3 bw = float3(v, v, v);
+
+				float4 result = c;
+				result.rgb = bw;
+				return result;
+			}
+			ENDCG
+		}
+	}
+}
